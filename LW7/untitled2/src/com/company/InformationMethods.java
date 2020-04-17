f9a7d8b03aae19f773a39eaa5de61cfb041130d2
Alexander <sanyo.ck@yandex.ru>
2020-04-17 15:02:07 +0300

    .

---
+++ LW7/untitled2/src/com/company/InformationMethods.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface InformationMethods {
+    public abstract String toString();
+    public abstract String getProfession();
+    public abstract int getZP();
+    public abstract  String toString2();
+}
